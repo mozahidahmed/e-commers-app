37ec3db11ac66f90cccece02824e38947c169052
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-03-25 16:45:30 +0600

    fetch use

--- src/component/pages/Teacher/SearchTeacher.js
+++ src/component/pages/Teacher/SearchTeacher.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 const SearchTeacher = () => {
+
+  const [data, setData] = useState([]);
   const [userData, setUserData] = useState([]);
   const [search, setSearch] = useState([]);
   const [division, setDivision] = useState("");
   const [zilla, setZilla] = useState("");
 
   useEffect(() => {
-    const data = [
-      { zilla: "MozahidD", division: "Dhaka" },
-      { zilla: "MozahidD", division: "Dhaka" },
-      { zilla: "sahporan", division: "Sylhet" },
-      { zilla: "kadimpara", division: "Sylhet" },
-      { zilla: "kadimpara", division: "Sylhet" },
-      { zilla: "MozahidB", division: "Barisal" },
-      { zilla: "MozahidK", division: "Khulna" },
-      { zilla: "MozahidK", division: "Khulna" },
-    ];
-    setUserData(data);
-    setSearch(data);
+
+      fetch("Data.json")
+      .then((res) => res.json())
+      .then((data) => 
+      setUserData(data))
+     
+
+       
   }, []);
 
   const handleSearch = () => {
     const newData = userData
       .filter((y) => y.zilla === (zilla === "" ? y.zilla : zilla))
       .filter((y) => y.division === (division === "" ? y.division : division));
-    setSearch(newData);
+       setSearch(newData);
+   
+    
   };
 
   return (
-    <div className="py-6 ">
+    <div className="py-6">
       <div className="grid justify-center ">
         <div className="grid sm:grid-cols-4  md:grid-cols-2 lg:grid-cols-5 gap-6   justify-center items-center">
           <div className="">
@@ -106,28 +106,36 @@ const SearchTeacher = () => {
       </div>
 
       <div className="grid py-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  px-6 justify-center items-center">
-        {search.slice(0, 6).map((data) => (
-          <>
-            <div className="card glass card-compact w-96 bg-base-100 shadow-xl  p-6">
-              <div className="avatar grid justify-center ">
-                <div className="w-32 rounded-full  ring ring-primary ring-offset-base-100 ring-offset-2">
-                  <img
-                    src="https://templatekit.mattsapii.co/padora/wp-content/uploads/sites/5/2020/12/group-of-diverse-students-at-daycare-PEBBJWW.jpg"
-                    alt="ghf"
-                  />
+        {search && search.length > 0 ? (
+          search.slice(0, 6).map((data) => (
+            <>
+              <div className="card glass card-compact w-96 bg-base-100 shadow-xl  p-6">
+                <div className="avatar grid justify-center ">
+                  <div className="w-32 rounded-full  ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img
+                      src="https://templatekit.mattsapii.co/padora/wp-content/uploads/sites/5/2020/12/group-of-diverse-students-at-daycare-PEBBJWW.jpg"
+                      alt="ghf"
+                    />
+                  </div>
+                </div>
+                <div className="text-center py-6 px-6">
+                  <h1 className="text-2xl font-bold">Mozahid</h1>
+                  <h1 className="text-2xl">{data.zilla} </h1>
+                  <p>most experties teaher in sylhet</p>
+                  <div className="hover:bg-primary rounded-full p-2 mt-2">
+                    <h1 className="text-primary font-bold hover:text-white">
+                      See More
+                    </h1>
+                  </div>
                 </div>
               </div>
-              <div className="text-center py-6 px-6">
-                <h1 className="text-2xl font-bold">Mozahid</h1>
-                <h1 className="text-2xl">English </h1>
-                <p>most experties teaher in sylhet</p>
-                <div className="hover:bg-primary rounded-full p-2 mt-2">
-                  <h1 className="text-primary font-bold hover:text-white">See More</h1>
-                </div>
-              </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))
+        ) : (
+          <div className="grid justify-center">
+            <h1 className="text-4xl">% search_cerfull</h1>
+          </div>
+        )}
       </div>
     </div>
   );
